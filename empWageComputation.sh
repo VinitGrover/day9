42eb00bcac643386e833e804873044cc0ee053b6
VinitGrover <vinitgrover255@gmail.com>
2022-05-10 16:41:28 +0530

    modified : added uc5

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,16 @@
 echo "Welcome to Employee Wage Computation Program"
+
+totalWage=0
+for ((count=1;count<=20;count++))
+do
+x=$((RANDOM%2))
+case $x in
+0)
+wage=$((4*20)) ;;
+1)
+wage=$((8*20)) ;;
+esac
+totalWage=$(($totalWage+$wage))
+done
+
+echo $totalWage

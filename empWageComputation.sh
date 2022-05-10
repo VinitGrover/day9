1e01833b42b6df3dc8c08a214893a52498ebece5
VinitGrover <vinitgrover255@gmail.com>
2022-05-10 15:02:20 +0530

    modified: uc2.2

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,22 +1,7 @@
 echo "Welcome to Employee Wage Computation Program"
 
 wagePerHour=20
-function func() {
-case $1 in
-0)
-workingHour=0 ;;
-1)
-workingHour=4 ;;
-2)
-workingHour=8 ;;
-esac
-echo $workingHour
-}
-
 for ((count=1;count<=30;count++))
 do
-x=$(func $((RANDOM%3)))
-arr[$(($count-1))]=$(($x*$wagePerHour))
+echo  "daily wage is " $(($wagePerHour*8))
 done
-
-echo ${arr[@]}

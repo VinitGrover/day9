8ba459ba1c48e98787be53383022355badc26e44
VinitGrover <vinitgrover255@gmail.com>
2022-05-10 16:30:46 +0530

    Modified: added uc3

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,13 @@
 echo "Welcome to Employee Wage Computation Program"
+
+for ((count=1;count<=30;count++))
+do
+x=$((RANDOM%2))
+if [ $x == 0 ]
+then
+echo $((4*20))
+else
+echo $((8*20))
+fi
+done
+
